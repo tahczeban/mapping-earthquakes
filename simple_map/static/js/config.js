0018de6a2e2397de71f0d009d700a45f4620056a
tahczeban <90135381+tahczeban@users.noreply.github.com>
2022-01-29 13:17:38 -0500

    update .js files

--- simple_map/static/js/config.js
+++ simple_map/static/js/config.js
@@ -1,1 +1,1 @@
-pk.eyJ1IjoidGFoY3plYmFuIiwiYSI6ImNreXN4dGRnZDE3dTcydW83bGttMnVmNzgifQ.XzEf0JEXlX7NmFlQI-bFEw
+const API_KEY="pk.eyJ1IjoidGFoY3plYmFuIiwiYSI6ImNreXN4dGRnZDE3dTcydW83bGttMnVmNzgifQ.XzEf0JEXlX7NmFlQI-bFEw";
